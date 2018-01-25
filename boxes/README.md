b75e2f13c6157e0d9aa0fd20722e0e421c17403c
chirangaalwis <chirangaalwis@gmail.com>
2018-01-25 12:18:06 +0530

    Fix conflicts in .gitignore

---
+++ boxes/README.md
@@ -0,0 +1,36 @@
+# Vagrant box generation
+
+This section defines the procedure to build Vagrant boxes required for WSO2 Identity Server 5.4.0 single node
+deployment with Analytics support.
+
+Please note that in order to build these Vagrant boxes, you need to install Ruby and
+[Oracle VM VirtualBox](http://www.oracle.com/technetwork/server-storage/virtualbox/downloads/index.html),
+since, Vagrant uses Oracle VM VirtualBox as the default provider.
+
+## How to build the Vagrant boxes
+
+![Vagrant box build process](procedure.png)
+
+##### 1. Checkout this repository into your local machine using the following Git command.
+```
+git clone https://github.com/wso2-incubator/vagrant-is.git
+```
+
+##### 2. Move to `boxes` folder.
+
+    cd boxes
+
+##### 3. Execute the build.sh shell script.
+
+    ./build.sh
+    
+##### 4. Add created box files to local Vagrant box cache.
+
+The created box files can be found in the output directory. In order to add a created box to the local Vagrant box cache use the `vagrant box add` command.
+
+    vagrant box add <BOX_NAME> <BOX_FILE_PATH>
+
+An example for adding the created WSO2 Identity Server Vagrant box file (by default, defined
+within the `config.yaml` file) is as follows:
+    
+    vagrant box add wso2is output/wso2is.box
---
+++ boxes/identity-server/provisioner/provisioner.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# Copyright 2017 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+# ------------------------------------------------------------------------
+
+# This script acts as the machine provisioner during the Vagrant box build process for WSO2 Identity Server.
+
+# set variables
+WSO2_SERVER=wso2is
+WSO2_SERVER_VERSION=5.4.0
+WSO2_SERVER_PACK=${WSO2_SERVER}-${WSO2_SERVER_VERSION}*.zip
+JDK_ARCHIVE=jdk-8u*-linux-x64.tar.gz
+
+DEFAULT_MOUNT=/vagrant
+SOFTWARE_DISTRIBUTIONS=${DEFAULT_MOUNT}/files
+CONFIGURATIONS=${DEFAULT_MOUNT}/identity-server/confs
+WORKING_DIRECTORY=/home/vagrant
+JAVA_HOME=/opt/java
+DEFAULT_USER=vagrant
+
+# operate in anti-fronted mode with no user interaction
+export DEBIAN_FRONTEND=noninteractive
+
+# check if the required software distributions have been added
+if [ ! -f ${SOFTWARE_DISTRIBUTIONS}/${WSO2_SERVER_PACK} ]; then
+    echo "WSO2 server pack not found. Please copy the ${WSO2_SERVER_PACK} to ${SOFTWARE_DISTRIBUTIONS} folder and retry."
+    exit 1
+fi
+
+if [ ! -f ${SOFTWARE_DISTRIBUTIONS}/${JDK_ARCHIVE} ]; then
+    echo "JDK archive file not found. Please copy the JDK archive file to ${SOFTWARE_DISTRIBUTIONS} folder and retry."
+    exit 1
+fi
+
+echo "Starting the ${WSO2_SERVER}-${WSO2_SERVER_VERSION} Vagrant box build process..."
+
+# install utility software
+echo "Installing software utilities..."
+apt-get install unzip
+echo "Successfully installed software utilities"
+
+# set up Java
+echo "Setting up Java..."
+if test ! -d ${JAVA_HOME}; then mkdir ${JAVA_HOME}; fi
+if test -d ${JAVA_HOME}; then
+  tar -xf ${SOFTWARE_DISTRIBUTIONS}/${JDK_ARCHIVE} -C ${JAVA_HOME} --strip-components=1
+fi
+echo "Successfully set up Java"
+
+# unpack the WSO2 product pack to the working directory
+echo "Setting up the ${WSO2_SERVER}-${WSO2_SERVER_VERSION} server..."
+if test ! -d ${WSO2_SERVER}-${WSO2_SERVER_VERSION}; then
+  unzip -q ${DEFAULT_MOUNT}/files/${WSO2_SERVER_PACK} -d ${WORKING_DIRECTORY}
+fi
+echo "Successfully set up ${WSO2_SERVER}-${WSO2_SERVER_VERSION} server"
+
+# set ownership of the working directory to the default ssh user and group
+chown -R ${DEFAULT_USER}:${DEFAULT_USER} ${WORKING_DIRECTORY}
+
+# remove the APT cache
+apt-get clean
+
+# zero out the drive
+dd if=/dev/zero of=/dev/sdX
+rm -f /dev/sdX
+
+# clear the bash history and exit
+cat /dev/null > ${WORKING_DIRECTORY}/.bash_history && history -c
